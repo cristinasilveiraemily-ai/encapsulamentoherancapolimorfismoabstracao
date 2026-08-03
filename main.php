4941b784ebbab1a281228f536462c93a832d8185
emily cristina da silveira <cristina.silveira.emily@escola.pr.gov.br>
2026-08-03 14:57:48 +0000

    oioi

--- main.php
+++ main.php
@@ -197,3 +197,36 @@
 // echo $joao->mostrarFuncionario();
 
 // ?>
+  <?php
+
+//4
+
+// class Pessoa{
+//     protected $nome;
+//     protected $idade;
+
+//     public function __construct($nome, $idade){
+//         $this->nome = $nome;
+//         $this->idade = $idade;
+//     }
+
+//     public function setNome($nome){
+//         $this->nome = $nome;
+//     }
+
+//     public function getNome(){
+//         return $this->nome;
+//     }
+
+//     public function setIdade($idade){
+//         $this->idade = $idade;
+//     }
+
+//     public function getIdade(){
+//         return $this->idade;
+//     }
+
+//     public function apresentar(){
+//         return "Nome: $this->nome\nIdade: $this->idade\n";
+//     }
+// }
